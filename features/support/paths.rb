5ec7ba25b6f69a9f3cae7a8dd3e76fe598c2027d
ctayl138 <ctayl138@uncc.edu>
2015-12-05 20:09:47 +0000

    scenario 1 complete

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -16,11 +16,8 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/'
 
-    # Add more mappings here.
-    # Here is an example that pulls values out of the Regexp:
-    #
-    #   when /^(.*)'s profile page$/i
-    #     user_profile_path(User.find_by_login($1))
+    when /^the edit page for "([^"]+)"$/
+      edit_movie_path(Movie.find_by_title($1).id)
 
     else
       begin
